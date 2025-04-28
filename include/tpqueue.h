9865c024dfdb51c1d9e90a57b2dccbf8f4a60992
Павел Золин <pavelzolin@MacBook-Pro-Pavel.local>
2025-04-29 00:24:38 +0300

    реализация TPQueue

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,58 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <stdexcept>
+
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct QueueNode {
+    T queueData;
+    QueueNode* nextNode;
+    explicit QueueNode(const T& val) : queueData(val), nextNode(nullptr) {}
+  };
+  QueueNode* queueHead;
+
+ public:
+  TPQueue() : queueHead(nullptr) {}
+
+  ~TPQueue() {
+    while (queueHead) {
+      QueueNode* nodeToDelete = queueHead;
+      queueHead = queueHead->nextNode;
+      delete nodeToDelete;
+    }
+  }
+
+  void enqueue(const T& item) {
+    QueueNode* newNode = new QueueNode(item);
+    if (!queueHead) {
+      queueHead = newNode;
+      return;
+    }
+    if (item.prior > queueHead->queueData.prior) {
+      newNode->nextNode = queueHead;
+      queueHead = newNode;
+      return;
+    }
+    QueueNode* currentNode = queueHead;
+    while (currentNode->nextNode && currentNode->nextNode->queueData.prior >= item.prior) {
+      currentNode = currentNode->nextNode;
+    }
+    newNode->nextNode = currentNode->nextNode;
+    currentNode->nextNode = newNode;
+  }
+
+  T dequeue() {
+    if (!queueHead) {
+      throw std::out_of_range("Queue is empty!");
+    }
+    QueueNode* nodeToRemove = queueHead;
+    T tempValue = queueHead->queueData;
+    queueHead = queueHead->nextNode;
+    delete nodeToRemove;
+    return tempValue;
+  }
 };
 
 struct SYM {
@@ -13,3 +62,4 @@ struct SYM {
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
+
